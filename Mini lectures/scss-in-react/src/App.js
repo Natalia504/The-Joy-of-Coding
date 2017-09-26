446c0228984b5278e7550b771a134b0ae2bfcb96
Todd Rasband <todd.rasband@gmail.com>
2017-09-26 00:35:41 -0700

    First draft

--- Mini lectures/scss-in-react/src/App.js
+++ Mini lectures/scss-in-react/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -10,9 +9,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="text-holder">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="flex">
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
+        <div className="item"></div>
       </div>
     );
   }
