e9ce4a058346a32346e831406f4640db14f88d14
Todd Rasband <todd.rasband@gmail.com>
2017-11-02 16:34:19 -0700

    fixed browser storage

--- Toy Problems/Medium/lowestCommonDenominator.js
+++ Toy Problems/Medium/lowestCommonDenominator.js
@@ -8,28 +8,63 @@
 
 // Do not assume there will be only 3 fractions.  Your function will need to account for an unknown number of nested arrays.
 
-var fractions = [[1, 2], [1, 3], [1, 4]];
-var fractionsTwo = [[3,7],[8,12],[7,45]];
+const fractionsOne = [[1, 2], [1, 3], [1, 4]];
+const fractionsTwo = [[3, 7], [8, 12], [7, 45]];
 
-function lowestCommonDenominator (fractions) {
-  var foundDenominator = false;
-  for (var i = 1; foundDenominator === false; i++) {
-    var tracker = 0;
-    for (var j = 0; j < fractions.length; j++) {
-      if(i % fractions[j][1] === 0) {
-        tracker++;
-      }
+function LCD(fractions) {
+  let largestDenominator = 0;
+  fractions.forEach((fraction) => {
+    if(fraction[1] > largestDenominator) {
+      largestDenominator = fraction[1];
     }
-    if (tracker === fractions.length) {
-      foundDenominator = true;
-      for (var k = 0; k < fractions.length; k++) {
-        fractions[k][0] = (i / fractions[k][1]) * fractions[k][0];
-        fractions[k][1] = (i / fractions[k][1]) * fractions[k][1];
+  });
+  let denominatorsAreDivisibleByCounter = 0;
+  let currentLcdToCheck = largestDenominator;
+  const numberOfFractions = fractions.length;
+  do {
+    denominatorsAreDivisibleByCounter = 0;
+    fractions.forEach((fraction) => {
+      const fractionsDenominator = fraction[1];
+      if (currentLcdToCheck % fractionsDenominator === 0) {
+        denominatorsAreDivisibleByCounter += 1;
       }
-      return  fractions
+    });
+    if(denominatorsAreDivisibleByCounter !== numberOfFractions) {
+      denominatorsAreDivisibleByCounter = 0;
+      currentLcdToCheck += largestDenominator;
     }
-  }
+  } while (denominatorsAreDivisibleByCounter !== numberOfFractions);
+  return fractions.map((fraction) => {
+    const numberToMultiplyBy = currentLcdToCheck / fraction[1];
+    fraction[0] = numberToMultiplyBy * fraction[0];
+    fraction[1] = numberToMultiplyBy * fraction[1];
+    return fraction;
+  });
 }
+const x = LCD(fractionsOne);
+x;
+const y = LCD(fractionsTwo);
+y;
 
-lowestCommonDenominator(fractions);
-lowestCommonDenominator(fractionsTwo)
+// function lowestCommonDenominator (fractions) {
+//   var foundDenominator = false;
+//   for (var i = 1; foundDenominator === false; i++) {
+//     var tracker = 0;
+//     for (var j = 0; j < fractions.length; j++) {
+//       if(i % fractions[j][1] === 0) {
+//         tracker++;
+//       }
+//     }
+//     if (tracker === fractions.length) {
+//       foundDenominator = true;
+//       for (var k = 0; k < fractions.length; k++) {
+//         fractions[k][0] = (i / fractions[k][1]) * fractions[k][0];
+//         fractions[k][1] = (i / fractions[k][1]) * fractions[k][1];
+//       }
+//       return  fractions
+//     }
+//   }
+// }
+
+// lowestCommonDenominator(fractions);
+// lowestCommonDenominator(fractionsTwo)
